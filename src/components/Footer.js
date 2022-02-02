42f46466d3fd955deb5c2996529d27bc922773ab
Ea Francisco <emberkasai04@gmail.com>
2022-02-02 11:26:10 -0500

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return(
+        <footer>
+            <p>Copyrighted by Elizabeth Francisco. All rights reserved</p>
+        </footer>
+            )
+    } 
+}
+
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import "./css/style.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Header />
+
+    <Footer />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
