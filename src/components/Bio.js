3ef6463efb7d92f637425fb54ad1395aef69f9a6
Ea Francisco <emberkasai04@gmail.com>
2022-02-02 15:05:21 -0500

    add more info on About me

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,18 +4,28 @@ function Bio(){
     return(
     /*html here */
         <main>
-            <div class ="bio">
-                <div class ="about">
+            <div class ="intro">
+                <div>
                     <h2>Hi! I'm Elizabeth Francisco.</h2>
                     <h3>Welcome to my sample website.</h3>
                     <br></br>
 
-                    <p class="intro">I am a freelance writer and content manager from the Philippines. This project was created to help us practice making single web pages using React.
+                    <p>This project was created to help us practice making single web pages using React.
                     </p>
                 </div>
 
                 <img src={profilepic} alt="Elizabeth Profile" id="profile" />
             </div>
+
+            <div class="about">
+                <h2> About Me</h2>
+                <p>
+                    I am a freelance writer and content manager from the Philippines. I have over 4 years of experience helping clients build their brand and growing the social media.
+                    Part of my work also involved creating and handlnig their websites. It was through my work in digital marketing that I was introduced to the idea of pursuing web design.
+                </p>
+
+            </div>
+            
             
         </main>
 
