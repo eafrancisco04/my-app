428c3c644fb78111ad461f9c1fad714057f2be2c
Ea Francisco <emberkasai04@gmail.com>
2022-02-02 11:23:24 -0500

    create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header>
+            <ul class="menu">
+                <li>Profile</li>
+                <li>Portfolio</li>
+                <li>Background</li>
+                <li>Contact Me</li>
+            </ul>
+        </header>
+        )
+    } 
+}
+
+
+export default Header
