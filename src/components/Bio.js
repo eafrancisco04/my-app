1be88f22310a1869ab9c8163273d99932bf81da0
Ea Francisco <emberkasai04@gmail.com>
2022-02-02 11:41:20 -0500

    Add picture of self in bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,14 +1,20 @@
+import profilepic from '../img/profilepic.jpg'
+
 function Bio(){
     return(
     /*html here */
         <main>
             <div class ="bio">
-                <h2>My name is Elizabeth Ann Francisco</h2>
-                <h3> Welcome to my sample portfolio website.</h3>
+                <h2>Hi! I'm Elizabeth Francisco.</h2>
+                <h3>Welcome to my sample website.</h3>
                 <br></br>
-                <p class="intro">I am a freelance writer and content manager from the Philippines. I am currently studying Web Design and Development at Humber College. I have a bachelor's degree in International Studies, major in European Studies from De La Salle University.
+
+                <p class="intro">I am a freelance writer and content manager from the Philippines. This project was created to help us practice making single web pages using React.
                 </p>
             </div>
+
+            <img src={profilepic} alt="Elizabeth Profile" />
+            
         </main>
 
     )
