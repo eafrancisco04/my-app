81ff780c6c8d0e8be147aa4377f4182a026d0328
Ea Francisco <emberkasai04@gmail.com>
2022-02-02 11:29:15 -0500

    Create bio component

---
+++ src/components/Bio.js
@@ -0,0 +1,19 @@
+function Bio(){
+    return(
+    /*html here */
+        <main>
+            <div class ="bio">
+                <h2>My name is Elizabeth Ann Francisco</h2>
+                <h3> Welcome to my sample portfolio website.</h3>
+                <br></br>
+                <p class="intro">I am a freelance writer and content manager from the Philippines. I am currently studying Web Design and Development at Humber College. I have a bachelor's degree in International Studies, major in European Studies from De La Salle University.
+                </p>
+            </div>
+        </main>
+
+    )
+
+}
+
+
+export default Bio
