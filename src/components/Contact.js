2d71881b810e1c1954f4e335439729b8137067a8
Ea Francisco <emberkasai04@gmail.com>
2022-02-02 11:29:52 -0500

    Create Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+function Contact(){
+    return(
+    /*html here */
+        <main>
+            <div class ="bio">
+                <h2>Contact Me</h2>
+                
+            </div>
+        </main>
+
+    )
+
+}
+
+
+export default Contact
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Bio from './components/Bio'
+import Contact from './components/Contact'
 import "./css/style.css"
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Bio />
-
+    <Contact />
     <Footer />
 
   </React.StrictMode>,
