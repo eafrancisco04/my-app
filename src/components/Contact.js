365983dbdfb4e67052d8a552a7789438383550e8
Ea Francisco <emberkasai04@gmail.com>
2022-02-02 13:04:10 -0500

    ass  styling to inpur box

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,16 @@ function Contact(){
     /*html here */
         <main>
             <div class ="bio">
-                <h2>Contact Me</h2>
+                <form id="form">   
+                    <h2>Contact Me</h2>
+
+                    <input class="nameemail" type="text" placeholder="Your Name" id="name"></input>
+                    <input class="nameemail" type="email" placeholder="Your Email" id="Email"></input>
+                    <br/>
+                    <input class="message" type="" placeholder="Your message" id="message"></input>
+                    <br/>
+                    <button type="submit">Submit</button>
+                 </form>  
                 
             </div>
         </main>
